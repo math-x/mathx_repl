3e6762d5e19584bd611e604d5ab6c56acb160491
kanav99 <kanav0610@gmail.com>
2018-07-08 23:01:56 +0530

    Adds color to error

--- src/error.cpp
+++ src/error.cpp
@@ -5,28 +5,28 @@
  */
 void error_handler (int x) {
 	if (x == 1) {
-		std::cout << "Error: Function not valid";
+		std::cout << "\033[1;31mError\033[0m: Function not valid";
 	}
 	else if (x == 2) {
-		std::cout << "Error: Variable not declared or invalid";
+		std::cout << "\033[1;31mError\033[0m: Variable not declared or invalid";
 	}
 	else if (x == 3) {
-		std::cout << "Error: Variable not declared";
+		std::cout << "\033[1;31mError\033[0m: Variable not declared";
 	}
 	else if (x == 4) {
-		std::cout << "Error: Multiple decimal points in number";
+		std::cout << "\033[1;31mError\033[0m: Multiple decimal points in number";
 	}
 	else if (x == 5) {
-		std::cout << "Error: Incorrect number of arguments";
+		std::cout << "\033[1;31mError\033[0m: Incorrect number of arguments";
 	}
 	else if (x == 6) {
-		std::cout << "Error: Invalid number or order of parentheses";
+		std::cout << "\033[1;31mError\033[0m: Invalid number or order of parentheses";
 	}
 	else if (x == 7) {
-		std::cout << "Error: Constant value cannot be changed";
+		std::cout << "\033[1;31mError\033[0m: Constant value cannot be changed";
 	}
 	else {
-		std::cout << "Error: Unknown error";
+		std::cout << "\033[1;31mError\033[0m: Unknown error";
 	}
 
 	if(is_silenced)
